7f9dbaee39f6df938662ef2ddd586009725b9b66
Nate Sargent <nate.sargent@illumisoft.com>
2018-01-23 20:07:47 -0600

    added bs auth variable to state

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,46 +1,54 @@
 import React from "react";
-import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { ListItem, Header, Left, Body, Right, Title, Button } from "native-base";
-import { WebBrowser } from "expo";
+import {Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {ListItem, Header, Left, Body, Right, Title, Button} from "native-base";
+import {WebBrowser} from "expo";
 import * as firebase from "firebase";
 
 export default class HomeScreen extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			byteCounter: 0
-		};
-	}
+    this.state = {
+      byteCounter: 0,
+      authorized: false
+    };
+  }
 
-	static navigationOptions = {
-		header: null
-	};
+  static navigationOptions = {
+    header: null
+  };
 
-	renderItem(item) {
-		return <ListItem item={item} onPress={() => {}} />;
-	}
+  renderItem(item) {
+    return <ListItem item={item} onPress={() => {}} />;
+  }
 
-	render() {
-		console.log(this.state.byteCounter);
-		return (
-			<View>
-				<Header>
-					<Left>
-						<Text>Left</Text>
-					</Left>
-					<Body>
-						<Title>FanTasktic</Title>
-					</Body>
-					<Right>
-						<Text>Right</Text>
-					</Right>
-				</Header>
-				<Text>{this.state.byteCounter}</Text>
-				<Button style={{ padding: 10 }} onPress={() => this.setState({ byteCounter: this.state.byteCounter + 1 })}>
-					<Text>Click Me!</Text>
-				</Button>
-			</View>
-		);
-	}
+  render() {
+    const {authorized, byteCounter} = this.state;
+    console.log(this.state.byteCounter);
+    return (
+      <View>
+        <Header>
+          <Left>
+            <Text>Left</Text>
+          </Left>
+          <Body>
+            <Title>FanTasktic</Title>
+          </Body>
+          <Right>
+            <Text>Right</Text>
+          </Right>
+        </Header>
+        <Text>{this.state.byteCounter}</Text>
+        <Button
+          style={{padding: 10}}
+          onPress={() => {
+            byteCounter >= 10 ? this.setState({authorized: true}) : null;
+            this.setState({byteCounter: byteCounter + 1});
+          }}>
+          <Text>Click Me!</Text>
+        </Button>
+        {authorized && <Text>You're logged in!</Text>}
+      </View>
+    );
+  }
 }
